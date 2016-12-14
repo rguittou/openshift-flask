d418e79313ce21e69b99ed8db9813dd24a43b367
stagiaire <rabahguittoune@gmail.com>
2016-12-14 14:19:48 +0100

    app /manager

--- app.py
+++ app.py
@@ -44,4 +44,4 @@ def index():
 
 
 if __name__ == '__main__':
-    manager.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT',8080)))
+    app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT',8080)))
